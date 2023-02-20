f6af81d0aef97fdaa76b9f867e730777c6180135
patrickmoder <patrick.moder@the-klu.org>
2023-02-20 11:34:26 +0100

    update

--- Decisions/__init__.py
+++ Decisions/__init__.py
@@ -19,13 +19,12 @@ def creating_session(subsession: Subsession):
 
     f = open(r"_static/treatments.csv", "r", encoding='utf-8-sig')
     rows = list(csv.DictReader(f))
-    #rowsr = itertools.cycle(rows)
+    rowsr = itertools.cycle(rows)
     players = subsession.get_players()
     #print(rows)
-    #for player in subsession.get_players():
-    for i in range(len(players)):
-        player = players[i]
-        row = rows[i]
+    for player in subsession.get_players():
+    #for i in range(len(players)):
+        row = next(rowsr)
         #print('treatment is', row)
         player.n_N = int(row['n_N'])
         player.n_P = int(row['n_P'])
