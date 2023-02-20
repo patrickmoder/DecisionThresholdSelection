306f429c989d3776d49008c02277782ced1d30a1
patrickmoder <patrick.moder@the-klu.org>
2023-02-20 11:08:41 +0100

    update

--- Decisions/__init__.py
+++ Decisions/__init__.py
@@ -23,7 +23,7 @@ def creating_session(subsession: Subsession):
 
     #print(rows)
     for player in subsession.get_players():
-        row =+ next(rowsr)
+        row += next(rowsr)
         #for i in range(len(players)):
         #p = players[i]
         #print('treatment is', row)
