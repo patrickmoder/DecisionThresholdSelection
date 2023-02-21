e1947e8d401aca1f8fc5c20189bb560534202196
patrickmoder <patrick.moder@the-klu.org>
2023-02-21 17:11:08 +0100

    update

--- PostExperiment/__init__.py
+++ PostExperiment/__init__.py
@@ -14,23 +14,31 @@ def creating_session(subsession: Subsession):
 class Group(BaseGroup):
     pass
 class Player(BasePlayer):
-    age = models.IntegerField(label='Your age?')
+    age = models.IntegerField(label='Your age?', min=18)
     similar_task = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], label='Have you ever done some similar task before?')
     choice_criterion = models.StringField(choices=[['cost', 'cost'], ['probability of positive characteristic', 'probability of positive characteristic'], ['probability of negative characteristic', 'probability of negative characteristic'], ['algorithm performance', 'algorithm performance'], ['other', 'other']], label='What criterion did you use to select the threshold?')
-    choice_criterion_add = models.StringField(label='Which additional criteria did you use to select the threshold?')
+    choice_criterion_add = models.StringField(label='Which additional criteria did you use to select the threshold?', blank=True)
     gender = models.StringField(choices=[['female', 'female'], ['male', 'male'], ['diverse', 'diverse'], ['do not want to disclose', 'do not want to disclose']], label='Your gender?')
     nationality = models.StringField(label='Your nationality?')
     study_progress = models.IntegerField(label='Your overall study progress in years?')
     professional_experience = models.IntegerField(label='Your professional experience in years?')
     highest_degree = models.StringField(choices=[['High School', 'High School'], ['Bachelors', 'Bachelors'], ['Masters', 'Masters'], ['Doctorate', 'Doctorate']], label='What is your highest degree in education?')
     strategy_change = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], label='Did you change your strategy to select the threshold during the experiment?')
-    strategy_how_why = models.LongStringField(label='If yes, why and how did you adjust your strategy?')
-    strategy_when = models.IntegerField(label='If yes, after how many rounds did you adjust the strategy?', max=C.NUM_ROUNDS, min=1)
+    strategy_how_why = models.LongStringField(label='If yes, why and how did you adjust your strategy?', blank=True)
+    strategy_when = models.IntegerField(label='If yes, after how many rounds did you adjust the strategy?', max=C.NUM_ROUNDS, min=1, blank=True)
     optimal_how_often = models.StringField(choices=[['never', 'never'], ['rarely', 'rarely'], ['sometimes', 'sometimes'], ['often', 'often'], ['always', 'always']], label='How often do you think that you selected the optimal threshold?')
     optimal_confident = models.IntegerField(label='How confident are you that you selected the optimal threshold in the majority of rounds in this experiment?', max=5, min=1)
     feedback_general = models.LongStringField(blank=True, label='Do you like to share any other feedback with the experimenters?')
     study_field = models.StringField(choices=[['Formal sciences (incl. mathematics, computer science, etc.)', 'Formal sciences (incl. mathematics, computer science, etc.)'], ['Natural sciences (incl. physics, engineering, medicine, etc.)', 'Natural sciences (incl. physics, engineering, medicine, etc.)'], ['Social sciences (incl. economics, psychology, jurisprudence, etc.)', 'Social sciences (incl. economics, psychology, jurisprudence, etc.)']], label='Your field of study?')
 class PostExperimentSurvey(Page):
     form_model = 'player'
-    form_fields = ['age', 'similar_task', 'gender', 'nationality', 'study_progress', 'professional_experience', 'highest_degree', 'study_field']
+    form_fields = ['age', 'gender', 'nationality', 'study_progress', 'highest_degree', 'study_field', 'professional_experience',
+                   'similar_task', 'optimal_confident', 'optimal_how_often',
+                   'choice_criterion', 'choice_criterion_add',
+                   'strategy_change', 'strategy_how_why', 'strategy_when',
+                   'feedback_general']
+
+class FinalPage(Page):
+    pass
+
 page_sequence = [PostExperimentSurvey]
