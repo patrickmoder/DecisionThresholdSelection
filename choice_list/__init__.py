c248882a997dc1b8741e55709a9a27a6069c80ab
patrickmoder <patrick.moder@the-klu.org>
2023-04-03 10:00:00 +0200

    update

--- choice_list/__init__.py
+++ choice_list/__init__.py
@@ -81,9 +81,8 @@ class Stimuli(Page):
         # if your page has a timeout, you would need to adjust this code.
         trials = Trial.filter(player=player)
         #choices = Trial.filter(player=player)
-        #choice10 = Trial.filter(player=player, sure_payoff='0.10cu')
-        if trials.sure_payoff == 0.10:
-            player.lottery10 = trials.chose_lottery
+        choice10 = Trial.filter(player=player, sure_payoff=0.10)
+        player.lottery10 = choice10.chose_lottery
 
         selected_trial = random.choice(trials)
         selected_trial.is_selected = True
